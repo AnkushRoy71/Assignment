e328814a1cdc972214aa4efb69b55822220fdd16
AnkushRoy71 <royankush832@gmail.com>
2026-04-17 16:35:29 +0530

    change in default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/auth/register/register.component';
+import { LoginComponent } from './features/auth/login/login.component';
 
 
 export const routes: Routes = [
-    {path: 'register', component: RegisterComponent},
-    {path: 'login', loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)},
-    {path: '', redirectTo: 'register', pathMatch: 'full'},
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/register/register.component').then(
+        (m) => m.RegisterComponent,
+      ),
+  },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
@@ -14,10 +15,11 @@ import { InputTextModule } from 'primeng/inputtext';
 export class LoginComponent {
 requiredPassword: string = 'Ab#123@';
 password: string = '';
+router = inject(Router);
 
 login(){
   if(this.password === this.requiredPassword){
-    alert('Login successful!');
+    this.router.navigate(['/register']);
   } else {
     alert('Invalid password. Please try again.');
   }
